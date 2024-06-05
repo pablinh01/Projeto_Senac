d3e08c49dcdacc7ff04c8bd257713d12faa21170
pablinh01 <pablohenriquetorresgama@gmail.com>
2024-06-05 11:35:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,116 +1,130 @@
-// Senac - taguatinga
-// Programacao Web com jks
-// conhecendo e explorando a linguagem
-// conhecndo as variaveis em js
+// coonhecendo o javascript
 
-let minhaidade = 30; //number
-let meunome = "pablo" //String
-let souestudante = true //Boolean
-let valornumlo = null //null
-let valorindefinido =  //Nao atribuir nenhum valor
-    let minhadatadenascimento = new Date(2024,07,06); //object
+// entender sobre funcioes basicas
 
-//
-let idade = 18;
-if (idade >= 18) {
-    console.log("voce pode votar");
-} else if (idade == 17) {
-    console.log("voce pode votar proximo ano");
+function saudacoes(nome) {
+    return "olá" + nome + "!";
 }
 
-let idade = 15;
-if (idade >= 18) {
-    console.log(" vocde pode votar");
-} else if (idade == 17) {
-    console.log("voce pode votar proximo ano");
-} else if {
-    console.log("voce ainda nao pode votar");
+// funcoes com multiplos parametros
+
+function soma(num1, num2) {
+    return num1 + num2;
 }
 
-let dia = 2;
-switch (dia) {
-    case 1:
-        console.log("domingo");
-        break;
-    case 2:
-        console.log("segunda");
-        break;
-    default:
-        console.log("dia invalido")
-}
+//funcoes com estrutura condicional
 
-//loop
-
-for (let d = 0; d < 5++) {
-    console.log(d);
-}
-
-let j = 1;
-while (i < 5) {
-    console.log(j);
-    i++;
-}
-let i = 0
-do {
-    console.log(i);
-    i++;
-} while (i < 5)
-
-//conversao de tipos de dados
-
-let num = 10;
-let str = "20"
-console.log(num + str); //resultarar em 30
-str = Number(str);
-console.log(num + str); //resultara em 30 (soma)
-
-//operadores 
-
-let a = 15;
-let b = 5;
-let c;
-c = a + b;
-console.log(c); //resultara em 20
-c = a - b;
-console.log(c); //resultara em 10
-c = a * b
-console.log(c); //resultara em 75
-c = a / b
-console.log(c); //resultara em 3
-
-//funsao senm js
-
-function nome(parami, parami, parami)
-//codigo de execusao
-
-function saldacao(nome) {
-    console.log("ola", + nome + "!")
-}
-let cor = "vermelho"; //variavel  global
-
-function mostrarcor() {
-    console.log(cor);
-}
-function mostrarcarro(); {
-    let carro = "ferrari"; //variavel local
-    console.log(carro);
-}
-mostrarcarro()  //imprime "ferrari"
-console.log(carro); //erro carr in not difind
-
-const number = [[1, 2, 3, 4, 5];
-
-
-const quadrados = number.map(function (n) {
-    return n * n;
-}
-console.log(quadrados); // imprime (1,2,3,15,25)
-function conprimentar(callback) {
-    console.log(Ola);
-    callback();
-    function dizernome() {
-        console.log("pablo");
-
+function parouimpar(num) {
+    if (num % 2 == 0) {
+        return "par";
+    } else {
+        return "impar";
     }
 }
-comprimentar(dizernome);
+
+//funcao com laco de repeticao
+
+function contagemregessiva(num) {
+    for (let i = num; i >= 0; i++) {
+        console.log(i);
+    }
+}
+
+//criando e manipulando objetos
+
+let carro = {
+    marca: "Nissan",
+    modelo: "versa",
+    ano: 2023,
+    ligar: function () {
+        console.log("O carro estar ligado");
+    }
+};
+
+// acessando as propriedade objeto 
+
+
+console.log(carro.marca); //mostre a marca do carro - Nissan
+console.log(carro['modelo']); // motre o modelo - Versa
+
+//manipular objetos
+
+carro.cor = "azul"
+console.log(carro.cor);
+
+carro.ano = 2023;
+console.log(carro.ano);
+
+delete carro.cor;
+console.log(carro.cor);
+
+//manipulando e criando arrays
+
+let numeros=[1,2,3,4,5,6];
+let nome = ["maria", "joao", "paulo", "lose"];
+let misto = [1,"j", true,[1,2],{nome: "joao", idade:30}];
+let frutas = ["banana", "maca", "pera", "cereja"]
+ console.log(frutas[0]);
+console.log(nome[1]);
+
+//alterando valores em arrays
+
+let frutas2 = ["banana", "maca", "pera", "cereja"];
+frutas[2] = "mamao"
+console.log(frutas);
+
+//metodos comuns de arrays
+//push(): adiciona um novo elemento no final do array
+//pop(): ele remove o ultimo elemento do array
+//shift(): remove o primeiro elemento do array
+//unshift(): adicional um novo elemento no inicio do array
+//slice(): ele cria um novo array com base em parte do array antigo
+//joing(): une todos os elementos do array em uma unica string
+//splice(): adiciona ou remove elemento de um array em uma posicao expecifica
+
+let frutas3 = ["banana", "maca", "pera", "cereja"];
+frutas3.push("uva");
+console.log(frutas3);
+
+frutas3.pop();
+console.log(frutas3);
+
+frutas.shift();
+console.log(frutas3);
+
+frutas3.unshift("morango");
+console.log(frutas3);
+
+frutas.splice(1,0,"kiwi");
+console.log(frutas3);
+
+let frutastropicais = frutas3.slice(0,2);
+console.log(frutastropicais);
+
+let algumasfrutas = frutas.slice(1,3);
+console.log(algumasfrutas);
+
+//metodos de intecacao : foreacht() - mat() - filter()
+
+let frutas4 = ["banana", "maca", "pera", "cereja"];
+frutas4.forEach(function(frutas4){
+    console.log(frutas4)
+});
+let numeros2 = [1,2,3,4,5,6];
+let quadrados = numeros2.map (function (numero2){
+return numeros2 * numeros2
+});
+console.log(quadrados);
+
+//Document : Object Model
+//document.getElementsByTagName(nome) document.querySelector(seletor) document.querySelectorAll(seletor)
+
+let heading = document.getElementById('main');
+heading.textContent = '<p> Este é um novo paragrafo';
+
+let div = document.getElementById('main');
+div.style.color = 'blue';
+
+
+
+ 
